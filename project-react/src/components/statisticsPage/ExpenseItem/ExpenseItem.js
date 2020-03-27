f79632060dd2e4f15300a2526f81c23dc631eec8
Andrei Dergavko <andrejdergavko@gmail.com>
2020-03-27 13:23:44 +0300

    finish commit

--- project-react/src/components/statisticsPage/ExpenseItem/ExpenseItem.js
+++ project-react/src/components/statisticsPage/ExpenseItem/ExpenseItem.js
@@ -6,12 +6,15 @@ import ProgressBox from "../ProgressBox/ProgressBox";
 
 function ExpenseItem(props) {
   const { color, title, currency, sum, ratio } = props;
+
+  const roundSum = Math.round(sum * 1000) / 1000;
+
   return (
     <div className="expenseItem">
       <div className="expenseItem__wrapper">
         <div className="expenseItem__header-box">
           <div className="expenseItem__header">{title}</div>
-          <div className="expenseItem__sum">{`${sum} ${currency}`}</div>
+          <div className="expenseItem__sum">{`${roundSum} ${currency}`}</div>
         </div>
         <ProgressBox ratio={ratio} color={color} />
       </div>
